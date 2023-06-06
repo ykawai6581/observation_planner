9e894e7e322908e734a502ea1412d48280561364
ykawai6581 <yugo6581@g.ecc.u-tokyo.ac.jp>
2023-06-06 14:49:00 +0900

    update

--- observation_planner.py
+++ observation_planner.py
@@ -434,7 +434,8 @@ with requests.Session() as s:
     constants['Moon azimuth'] = [moon_position(longitude,latitude,lst)['azimuth'] for lst in constants['Local sidereal time']]
 
     for i, plan in enumerate(plans):
-        plan = plan.sort_values(['Filler','Name','Priority'],ascending=[False,False,False]).reset_index()
+        plan = plan.sort_values(['Filler','Priority','Name'],ascending=[False,False,False])
+
         fig = plt.figure(figsize=(15,8))
         gs = gridspec.GridSpec(2, 2, width_ratios=[3, 1]) 
         ax_airmass_plot = plt.subplot(gs[0,0])
